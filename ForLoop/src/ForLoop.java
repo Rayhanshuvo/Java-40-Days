a406496c2a24f047a8cc0e385b46c793ad5bbf76
unknown <Rayhanshuvo>
2018-03-31 23:43:39 +0600

    1

--- ForLoop/src/ForLoop.java
+++ ForLoop/src/ForLoop.java
@@ -1,8 +1,15 @@
 
+
+ //Write a program in C to display the first 10 natural numbers
 public class ForLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=10;i++) {
+			System.out.println("First Ten natural  number is:"+i);
+			
+		}
 
 	}
 
